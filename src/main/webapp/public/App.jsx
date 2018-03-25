2d578799d3c12f0ea4d2fae493d4718ead49cd22
bwraight <b.r.wraight@gmail.com>
2018-03-25 18:09:13 +0100

    reformatting jsx

--- src/main/webapp/public/App.jsx
+++ src/main/webapp/public/App.jsx
@@ -1,121 +1,124 @@
 class App extends React.Component {
-  constructor(props) {
-      super(props);
-      this.deleteVehicle = this.deleteVehicle.bind(this);
-      this.rentVehicle = this.rentVehicle.bind(this);
-      this.releaseVehicle = this.releaseVehicle.bind(this);
-      this.createVehicle = this.createVehicle.bind(this);
-      this.toggleGridRenderFalse = this.toggleGridRenderFalse.bind(this);
-      this.state = {
-          vehicles: [],
-          customers: [],
-          shouldGridRender: true
-      };
-   }
+    constructor(props) {
+        super(props);
+        this.deleteVehicle = this.deleteVehicle.bind(this);
+        this.rentVehicle = this.rentVehicle.bind(this);
+        this.releaseVehicle = this.releaseVehicle.bind(this);
+        this.createVehicle = this.createVehicle.bind(this);
+        this.toggleGridRenderFalse = this.toggleGridRenderFalse.bind(this);
+        this.state = {
+            vehicles: [],
+            customers: [],
+            shouldGridRender: true
+        };
+    }
 
-  componentDidMount() {
-    this.loadCustomersFromServer();
-    this.loadVehiclesFromServer();
-  }
+    componentDidMount() {
+        this.loadCustomersFromServer();
+        this.loadVehiclesFromServer();
+    }
 
-  toggleGridRenderFalse(){
-    this.setState({
-        shouldGridRender: false
-    });
-  }
+    toggleGridRenderFalse() {
+        this.setState({
+            shouldGridRender: false
+        });
+    }
 
-  // Load vehicles from database
-  loadVehiclesFromServer() {
+    // Load vehicles from database
+    loadVehiclesFromServer() {
 
-      this.loadCustomersFromServer();
+        this.loadCustomersFromServer();
 
-      fetch('http://localhost:8080/api/vehicles')
-      .then((response) => response.json())
-      .then((responseData) => {
-          this.setState({
-              vehicles: responseData._embedded.vehicles,
-              shouldGridRender: true
-          });
-      });
-  }
+        fetch('http://localhost:8080/api/vehicles')
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    vehicles: responseData._embedded.vehicles,
+                    shouldGridRender: true
+                });
+            });
+    }
 
     // Load customers from database
     loadCustomersFromServer() {
         fetch('http://localhost:8080/api/customers')
-        .then((response) => response.json())
-        .then((responseData) => {
-            this.setState({
-                customers: responseData._embedded.customers,
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    customers: responseData._embedded.customers,
+                });
             });
-        });
     }
 
-  // Delete vehicle
-  deleteVehicle(vehicle) {
-      fetch (vehicle._links.self.href,
-      { method: 'DELETE',})
-      .then(
-          res => this.loadVehiclesFromServer()
-      )
-      .catch( err => console.error(err))
-  }
-
-  // Rent vehicle
-  //curl -i -X PUT -H "Content-Type:text/uri-list" -d "http://localhost:8080/api/Customers/1" http://localhost:8080/api/vehicles/1/customer
-  rentVehicle(vehicle, customer) {
-        fetch('http://localhost:8080/api/customers/search/findByEmail?email=' + customer)
-        .then((response) => response.json())
-        .then((responseData) => {
-            fetch(vehicle._links.customer.href, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'text/uri-list',
-                },
-                body: responseData._links.self.href
-            })
+    // Delete vehicle
+    deleteVehicle(vehicle) {
+        fetch(vehicle._links.self.href,
+            {method: 'DELETE',})
             .then(
                 res => this.loadVehiclesFromServer()
             )
-            .catch( err => console.error(err))
-        });
-   }
+            .catch(err => console.error(err))
+    }
+
+    // Rent vehicle
+    //curl -i -X PUT -H "Content-Type:text/uri-list" -d "http://localhost:8080/api/Customers/1" http://localhost:8080/api/vehicles/1/customer
+    rentVehicle(vehicle, customer) {
+        fetch('http://localhost:8080/api/customers/search/findByEmail?email=' + customer)
+            .then((response) => response.json())
+            .then((responseData) => {
+                fetch(vehicle._links.customer.href, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'text/uri-list',
+                    },
+                    body: responseData._links.self.href
+                })
+                    .then(
+                        res => this.loadVehiclesFromServer()
+                    )
+                    .catch(err => console.error(err))
+            });
+    }
 
     // Release vehicle
-      releaseVehicle(vehicle) {
-       fetch (vehicle._links.customer.href,
-             { method: 'DELETE',})
-             .then(
-                 res => this.loadVehiclesFromServer()
-             )
-             .catch( err => console.error(err))
+    releaseVehicle(vehicle) {
+        fetch(vehicle._links.customer.href,
+            {method: 'DELETE',})
+            .then(
+                res => this.loadVehiclesFromServer()
+            )
+            .catch(err => console.error(err))
 
-             this.loadVehiclesFromServer();
+        this.loadVehiclesFromServer();
 
-      }
+    }
 
-  // Create new vehicle
-  createVehicle(vehicle) {
-      fetch('http://localhost:8080/api/vehicles', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(vehicle)
-      })
-      .then(
-          res => this.loadVehiclesFromServer()
-      )
-      .catch( err => console.error(err))
-  }
+    // Create new vehicle
+    createVehicle(vehicle) {
+        fetch('http://localhost:8080/api/vehicles', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(vehicle)
+        })
+            .then(
+                res => this.loadVehiclesFromServer()
+            )
+            .catch(err => console.error(err))
+    }
 
-  render() {
-    return (
-       <div>
-          <VehicleForm createVehicle={this.createVehicle}/>
-          <VehicleTable deleteVehicle={this.deleteVehicle} rentVehicle={this.rentVehicle} releaseVehicle={this.releaseVehicle} toggleGridRenderFalse={this.toggleGridRenderFalse} vehicles={this.state.vehicles} customers={this.state.customers} shouldGridRender={this.state.shouldGridRender} />
-       </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <VehicleForm createVehicle={this.createVehicle}/>
+                <VehicleTable deleteVehicle={this.deleteVehicle} rentVehicle={this.rentVehicle}
+                              releaseVehicle={this.releaseVehicle} toggleGridRenderFalse={this.toggleGridRenderFalse}
+                              vehicles={this.state.vehicles} customers={this.state.customers}
+                              shouldGridRender={this.state.shouldGridRender}/>
+            </div>
+        );
+    }
 }
 
 class VehicleTable extends React.Component {
@@ -124,27 +127,30 @@ class VehicleTable extends React.Component {
     }
 
     render() {
-    var vehicles = this.props.vehicles.map(vehicle =>
-        <Vehicle key={vehicle._links.self.href} vehicle={vehicle} customers={this.props.customers} deleteVehicle={this.props.deleteVehicle} rentVehicle={this.props.rentVehicle} releaseVehicle={this.props.releaseVehicle} shouldGridRender={this.props.shouldGridRender} toggleGridRenderFalse={this.props.toggleGridRenderFalse}/>
-    );
+        var vehicles = this.props.vehicles.map(vehicle =>
+            <Vehicle key={vehicle._links.self.href} vehicle={vehicle} customers={this.props.customers}
+                     deleteVehicle={this.props.deleteVehicle} rentVehicle={this.props.rentVehicle}
+                     releaseVehicle={this.props.releaseVehicle} shouldGridRender={this.props.shouldGridRender}
+                     toggleGridRenderFalse={this.props.toggleGridRenderFalse}/>
+        );
 
-    return (
-      <div>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Vehicle Type</th>
-            <th>Make</th>
-            <th>Model</th>
-            <th>Number of Wheels</th>
-            <th>Number of Passengers</th>
-            <th>Rented by</th>
-          </tr>
-        </thead>
-        <tbody>{vehicles}</tbody>
-      </table>
-      </div>);
-  }
+        return (
+            <div>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Vehicle Type</th>
+                        <th>Make</th>
+                        <th>Model</th>
+                        <th>Number of Wheels</th>
+                        <th>Number of Passengers</th>
+                        <th>Rented by</th>
+                    </tr>
+                    </thead>
+                    <tbody>{vehicles}</tbody>
+                </table>
+            </div>);
+    }
 }
 
 class Vehicle extends React.Component {
@@ -161,8 +167,8 @@ class Vehicle extends React.Component {
 
 
         this.state = {
-                  customer: {}
-              };
+            customer: {}
+        };
     }
 
     handleChange(event) {
@@ -191,34 +197,35 @@ class Vehicle extends React.Component {
     loadRentalCustomer() {
 
         fetch(this.props.vehicle._links.customer.href)
-        .then((response) => response.json())
-        .then((responseData) => {
-            this.setState({
-                  customer: responseData,
-            });
-        })
-        .catch(err => console.error(err),
-                    this.setState({
-                          customer: {},
-                    })
-                )
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    customer: responseData,
+                });
+            })
+            .catch(err => console.error(err),
+                this.setState({
+                    customer: {},
+                })
+            )
 
     }
 
-    setupRentedBy(){
+    setupRentedBy() {
 
-        if(this.state.customer.email != null){
+        if (this.state.customer.email != null) {
             return (
                 <div>{this.state.customer.email}</div>
             );
         }
 
         var customers = this.props.customers.map(customer =>
-                        <option value={customer.id}>{customer.email}</option>
-                    );
+            <option value={customer.id}>{customer.email}</option>
+        );
 
         return (
-            <select className="custom-select my-1 mr-sm-2" id="customerSelect" name="customer" onChange={this.handleChange}>
+            <select className="custom-select my-1 mr-sm-2" id="customerSelect" name="customer"
+                    onChange={this.handleChange}>
                 <option selected>Choose...</option>
                 {customers}
             </select>
@@ -226,7 +233,7 @@ class Vehicle extends React.Component {
     }
 
     setupButtons() {
-        if(this.state.customer.email != null){
+        if (this.state.customer.email != null) {
             return (
                 <button className="btn btn-danger" onClick={this.releaseVehicle}>Release Rental</button>
             );
@@ -244,21 +251,21 @@ class Vehicle extends React.Component {
     }
 
     render() {
-        if(this.props.shouldGridRender){
+        if (this.props.shouldGridRender) {
             this.loadRentalCustomer();
             this.toggleGridRenderFalse();
         }
 
         return (
-          <tr>
-            <td>{this.props.vehicle.vehicleType}</td>
-            <td>{this.props.vehicle.make}</td>
-            <td>{this.props.vehicle.model}</td>
-            <td>{this.props.vehicle.numberOfWheels}</td>
-            <td>{this.props.vehicle.numberOfPassengers}</td>
-            <td>{this.setupRentedBy()}</td>
-            <td>{this.setupButtons()}</td>
-          </tr>
+            <tr>
+                <td>{this.props.vehicle.vehicleType}</td>
+                <td>{this.props.vehicle.make}</td>
+                <td>{this.props.vehicle.model}</td>
+                <td>{this.props.vehicle.numberOfWheels}</td>
+                <td>{this.props.vehicle.numberOfPassengers}</td>
+                <td>{this.setupRentedBy()}</td>
+                <td>{this.setupButtons()}</td>
+            </tr>
         );
     }
 }
@@ -272,7 +279,7 @@ class VehicleForm extends React.Component {
             model: '',
             numberOfWheels: '',
             numberOfPassengers: ''
-            };
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
@@ -287,11 +294,12 @@ class VehicleForm extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         console.log("Make: " + this.state.make);
-        var newVehicle = {  vehicleType: this.state.vehicleType,
-                            make: this.state.make,
-                            model: this.state.model,
-                            numberOfWheels: this.state.numberOfWheels,
-                            numberOfPassengers: this.state.numberOfPassengers
+        var newVehicle = {
+            vehicleType: this.state.vehicleType,
+            make: this.state.make,
+            model: this.state.model,
+            numberOfWheels: this.state.numberOfWheels,
+            numberOfPassengers: this.state.numberOfPassengers
         };
         this.props.createVehicle(newVehicle);
     }
@@ -301,34 +309,38 @@ class VehicleForm extends React.Component {
             <div className="panel panel-default">
                 <div className="panel-heading">Create vehicle</div>
                 <div className="panel-body">
-                <form className="form-inline">
-                    <div className="col-md-2">
-                        <select className="form-control" name="vehicleType" onChange={this.handleChange}>
-                            <option selected>Vehicle Type...</option>
-                            <option>CAR</option>
-                            <option>MOTORBIKE</option>
-                            <option>VAN</option>
-                            <option>CONVERTABLE</option>
-                        </select>
-                    </div>
-                    <div className="col-md-2">
-                        <input type="text" placeholder="Make" className="form-control"  name="make" onChange={this.handleChange}/>
-                    </div>
-                    <div className="col-md-2">
-                        <input type="text" placeholder="Model" className="form-control" name="model" onChange={this.handleChange}/>
-                    </div>
+                    <form className="form-inline">
+                        <div className="col-md-2">
+                            <select className="form-control" name="vehicleType" onChange={this.handleChange}>
+                                <option selected>Vehicle Type...</option>
+                                <option>CAR</option>
+                                <option>MOTORBIKE</option>
+                                <option>VAN</option>
+                                <option>CONVERTABLE</option>
+                            </select>
+                        </div>
+                        <div className="col-md-2">
+                            <input type="text" placeholder="Make" className="form-control" name="make"
+                                   onChange={this.handleChange}/>
+                        </div>
+                        <div className="col-md-2">
+                            <input type="text" placeholder="Model" className="form-control" name="model"
+                                   onChange={this.handleChange}/>
+                        </div>
 
-                    <div className="col-md-2">
-                        <input type="text" placeholder="Number of Wheels" className="form-control" name="numberOfWheels" onChange={this.handleChange}/>
-                    </div>
-                    <div className="col-md-2">
-                        <input type="text" placeholder="Number of Passengers" className="form-control" name="numberOfPassengers" onChange={this.handleChange}/>
-                    </div>
+                        <div className="col-md-2">
+                            <input type="text" placeholder="Number of Wheels" className="form-control"
+                                   name="numberOfWheels" onChange={this.handleChange}/>
+                        </div>
+                        <div className="col-md-2">
+                            <input type="text" placeholder="Number of Passengers" className="form-control"
+                                   name="numberOfPassengers" onChange={this.handleChange}/>
+                        </div>
 
-                    <div className="col-md-2">
-                        <button className="btn btn-success" onClick={this.handleSubmit}>Save</button>
-                    </div>
-                </form>
+                        <div className="col-md-2">
+                            <button className="btn btn-success" onClick={this.handleSubmit}>Save</button>
+                        </div>
+                    </form>
                 </div>
             </div>
 
@@ -336,4 +348,4 @@ class VehicleForm extends React.Component {
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root') );
+ReactDOM.render(<App />, document.getElementById('root'));
